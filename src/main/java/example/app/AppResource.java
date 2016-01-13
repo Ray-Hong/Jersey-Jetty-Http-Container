72215ae9ea631766b70d46fb1d7d037757f9c6cf
Ray-Hong <yhong315@gmail.com>
2016-01-13 12:01:44 -0800

    Update AppResource.java
    Update the file for testing.

--- src/main/java/example/app/AppResource.java
+++ src/main/java/example/app/AppResource.java
@@ -5,12 +5,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-@Path("/hello")
 public class AppResource {
     
     @GET
     @Produces("text/plain")
+    @Path("/hello")
     public Response getHello() {
         return Response.status(200).entity("Hello is called!").build();
+    }
+    
+    @GET
+    @Produces("text/plain")
+    @Path("/helloworld")
+    public Response getHellowWorld() {
+        return Response.status(200).entity("Hello world is called!").build();
     }
 }
